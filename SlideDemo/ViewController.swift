ac026e182bbb3360deee99ab498f570b62e973f7
CharlesYaoxin <Charlesyaoxin@163.com>
2016-09-12 12:55:05 +0800

    Initial Commit

---
+++ SlideDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SlideDemo
+//
+//  Created by Charles on 16/9/12.
+//  Copyright © 2016年 Charles. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
